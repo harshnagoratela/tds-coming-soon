d37cae136d8be7762e00a777db17883360836045
harshnagoratela <harshna.goratela@gmail.com>
2020-10-13 08:59:14 +0000

    Change bg image

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteTitle: 'The Digital Stride', // <title>
-  manifestName: 'TDS',
+  manifestName: 'The Digital Stride',
   manifestShortName: 'TDS', // max 12 characters
   manifestStartUrl: '/',
-  manifestBackgroundColor: '#30d1c1',
-  manifestThemeColor: '#30d1c1',
+  manifestBackgroundColor: '#348cb2',
+  manifestThemeColor: '#348cb2',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/tds-comming-soon/`, // This path is subpath of your hosting https://domain/portfolio
